182091e9c4a478ec90fd545a88e004864cf95141
Jeremy Dalcin <jbdalcin@gmail.com>
2019-09-20 21:32:01 -0400

    solution to EPI 5.1

---
+++ 5_1.cpp
@@ -0,0 +1,14 @@
+//
+//  5_1.cpp
+//  practice projects
+//
+//  Created by Bennie Dalcin on 9/20/19.
+//  Copyright © 2019 c++ practice. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello World! This is Bennie. Let's go!" << std::endl;
+    return 0;
+}
